ea30bcc793f3be33c7fef030fb9940a8eb74bd72
劉達 <ryu@anchorz.co.jp>
2021-03-07 16:48:27 +0900

    Update render.py

--- render.py
+++ render.py
@@ -28,21 +28,24 @@ class Render():
     def page(self, db, request, templates, link):
         # try:
             article = db.query(mdl.Article).filter(mdl.Article.link == link).first()
-            self.category_id = article.category_id
-            self.db = db
-            if article != None and os.path.exists("files/templates/article/show.html"):
-                data = {'request':request}
-                data['pageData'] = article.__dict__
-                data['prevData'] = article.__dict__
-                data['nextData'] = article.__dict__
-                data['image'] = self.image
-                data['category'] = self.category
-                data['DB_Search'] = self.db_search
-                print("正常")
-                return templates.TemplateResponse("article/show.html", data)
+            if article != None:
+                self.category_id = article.category_id
+                self.db = db
+                if article != None and os.path.exists("files/templates/article/show.html"):
+                    data = {'request':request}
+                    data['pageData'] = article.__dict__
+                    data['prevData'] = article.__dict__
+                    data['nextData'] = article.__dict__
+                    data['image'] = self.image
+                    data['category'] = self.category
+                    data['DB_Search'] = self.db_search
+                    print("正常")
+                    return templates.TemplateResponse("article/show.html", data)
+                else:
+                    print("检测出404" + str(article != None) + str(os.path.exists("files/templates/article/show.html")))
+                    return templates.TemplateResponse('404.html',{'request':request,'err':"no error"})
             else:
-                print("检测出404" + str(article != None) + str(os.path.exists("files/templates/article/show.html")))
-                return templates.TemplateResponse('404.html',{'request':request,'err':"no error"})
+                return templates.TemplateResponse('404.html',{'request':request,'err':"找不到文章"})
         # except Exception as e:
         #     print("发生错误")
         #     print(str(e))
