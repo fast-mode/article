9f7ab46ec15c8cdbc06795b768b4450172a3e030
劉達 <ryu@anchorz.co.jp>
2021-05-16 18:03:53 +0900

    u

--- __init__.py
+++ __init__.py
@@ -1,22 +1,20 @@
-from typing import List
-
 from fastapi import APIRouter
 from requests.sessions import Session
-
 from app.insmodes.article.route import article_route, article_page_route
 from app.models.module import ApiModule, PageModule, PageItem
 from app.models.page.crud import PageRouter
+from .mdl import Article as MdlArticle
 
 
-class Article(ApiModule, PageModule):
+class Article(PageModule, ApiModule):
     def _register_page_bp(self, bp: APIRouter, page_router: PageRouter):
         article_page_route(bp, page_router)
 
-    def get_pages(self, db: Session) -> List[PageItem]:
+    def _get_pages(self, db: Session) -> list:
         return []
 
     def __init__(self):
-        super(Article, self).__init__()
+        super().__init__()
 
     def _register_api_bp(self, bp: APIRouter):
         article_route(bp)
